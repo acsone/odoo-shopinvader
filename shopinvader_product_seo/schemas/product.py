1765b2217bc0b07b369cb86c53cd4951c3c5254e
Quentin Groulard <quentin.groulard@acsone.eu>
2023-10-04 17:48:37 +0200

    [IMP] Schema models import

--- shopinvader_product_seo/schemas/product.py
+++ shopinvader_product_seo/schemas/product.py
@@ -1,7 +1,7 @@
 # Copyright 2023 ACSONE SA/NV
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo.addons.shopinvader_product.schemas.product import (
+from odoo.addons.shopinvader_product.schemas import (
     ProductProduct as BaseShopinvaderVariant,
 )
 
